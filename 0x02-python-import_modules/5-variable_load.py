be057a496ecad866506e3a013502b597020fe9c0
ismailchiba <ismailchiba@gmail.com>
2023-09-13 23:25:05 +0100

    5 done

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    import sys
-    Len = len(sys.argv)
-    if Len == 1:
-        print("{} arguments.".format(Len - 1))
-    elif Len == 2:
-        print("{} argument:".format(Len - 1))
-    else:
-        print("{} arguments:".format(Len - 1))
-    for i in range(1, Len):
-        print("{}: {}".format(i, sys.argv[i]))
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
