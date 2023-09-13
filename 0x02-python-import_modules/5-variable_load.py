bd4e28af53916df746950dfc568191e5a8be9e44
ismailchiba <ismailchiba@gmail.com>
2023-09-13 23:21:38 +0100

    done

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argLen = len(sys.argv)
-    if argLen == 1:
-        print("{} arguments.".format(argLen - 1))
-    elif argLen == 2:
-        print("{} argument:".format(argLen - 1))
+    Len = len(sys.argv)
+    if Len == 1:
+        print("{} arguments.".format(Len - 1))
+    elif Len == 2:
+        print("{} argument:".format(Len - 1))
     else:
-        print("{} arguments:".format(argLen - 1))
-    for i in range(1, argLen):
+        print("{} arguments:".format(Len - 1))
+    for i in range(1, Len):
         print("{}: {}".format(i, sys.argv[i]))
