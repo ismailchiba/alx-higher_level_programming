5cc714746d769ade638b7f6f92e975723e20fd5c
root <root@DESKTOP-3D8FQDJ.localdomain>
2024-05-23 02:47:44 +0100

    form error

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
--- displays the number of records with id = 89 in the table first_table.
-select count(*) from first_table where id=89;
+-- Displays the number of records with id = 89 in the table first_table.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
