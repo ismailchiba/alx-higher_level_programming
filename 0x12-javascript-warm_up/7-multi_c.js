57f73bcff5270e50d6852d5b3f150859e1c11fef
root <root@DESKTOP-3D8FQDJ.localdomain>
2024-06-23 17:33:24 +0100

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const num = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(num)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
