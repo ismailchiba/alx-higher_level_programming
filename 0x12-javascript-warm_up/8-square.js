21ff553ef6c259170ea73b8c721874803805666f
root <root@DESKTOP-3D8FQDJ.localdomain>
2024-06-23 18:15:24 +0100

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function Square (num, txt) {
+  if (Number.isNaN(num)) {
+    console.log('Missing size');
+  } else if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      console.log(txt.repeat(num));
+    }
+  }
+}
+
+Square(Number.parseInt(process.argv[2]), '#');
