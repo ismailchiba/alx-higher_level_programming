c0cea8e1581617493ee3a87ce516c83b00816826
root <root@DESKTOP-3D8FQDJ.localdomain>
2024-06-23 15:07:10 +0100

    4-concat.js

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const arg0 = process.argv[2];
+const arg1 = process.argv[3];
+
+console.log((arg0 !== undefined ? arg0 : 'undifined') + ' is ' + (arg1 !== undefined ? arg1 : 'undifined'));
