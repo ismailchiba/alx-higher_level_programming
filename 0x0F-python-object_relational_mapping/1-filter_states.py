ca260c5fef5fa7993027a11b486f4cb800d68257
root <root@DESKTOP-3D8FQDJ.localdomain>
2024-06-27 00:32:17 +0100

    like binary N%

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                 ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
