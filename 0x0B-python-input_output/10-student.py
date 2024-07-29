2936b8c21c411e3d05055cf77b10c29b272ab079
= <=>
2024-07-30 00:50:52 +0100

    d

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,7 +17,7 @@ class Student:
         If attrs is a list of strings, represents only those attributes
         included in the list
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
