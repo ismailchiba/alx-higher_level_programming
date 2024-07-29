db885a7e9ef50436200be8024d7f528166d4797e
= <=>
2024-07-29 17:32:34 +0100

    in Python, True is a subclass of int

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """validates a value as an integer
         """
-        if not isinstance(value, int):
+        if not isinstance(value, int) or isinstance(value, bool):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
