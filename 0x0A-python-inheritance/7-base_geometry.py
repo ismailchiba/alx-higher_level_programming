f3cf049fb30c6330146952e4778ae1c65bf6a209
= <=>
2024-07-29 15:52:03 +0100

    update

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """validates a value as an integer
         """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
