4de61f906d168907e3e5b1d5f100d53636111ac4
= <=>
2024-08-01 01:18:33 +0100

    semistandard debuging

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 
-const request = require(`request`);
-const fs = require(`fs`);
+const request = require('request');
+const fs = require('fs');
 
-if (process.argv.length > 3){
-    const Url = process.argv[2];
-    const file = process.argv[3];
+if (process.argv.length > 3) {
+  const Url = process.argv[2];
+  const file = process.argv[3];
 
-    request(`${Url}`,(err, res, body) => {
-        if(err)
-        {
-            console.log(err);
-        }
-        fs.writeFile(`${file}`, body, 'utf8', (err) => {
-            if(err)
-            {
-                console.log(err);
-            }
-        })
-    })
+  request(`${Url}`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    }
+    fs.writeFile(`${file}`, body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
 }
